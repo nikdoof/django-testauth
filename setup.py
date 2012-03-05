79e967cc6f57c398f45226900b1301ea3ebb6a88
Andrew Williams <andy@tensixtyone.com>
2012-03-05 00:22:37 +0000

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+ 
+from distutils.core import setup
+ 
+setup(name = "django-testauth",
+    version = '0.1',
+    description = "Django library/application for authenticating against TEST services",
+    author = "Andrew Williams",
+    author_email = "matalok@pleaseignore.com",
+    url = "http://dev.pleaseignore.com/projects/django-dreddit",
+    keywords = "eve api eveonline",
+    packages = ['django-testauth',],
+)
